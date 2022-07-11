ab060502af9616091ef8521abd87966a2adbd5de
ullas <ullaskunder3@gmail.com>
2022-07-11 20:02:32 +0530

    add live-counter

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import styles from '../styles/Home.module.css'
 import fs from 'fs'
 import matter from 'gray-matter';
 import Link from 'next/link';
-import {Footer} from '../components/Footer';
+import { Footer } from '../components/Footer';
+import Image from 'next/image';
 
 export async function getStaticProps() {
   const files = fs.readdirSync('posts');
@@ -16,21 +17,24 @@ export async function getStaticProps() {
       slug, frontMatter
     }
   })
+  const response = await fetch('https://api.countapi.xyz/update/ullaskunder3/profileView/?amount=1')
+  const data = await response.json()
   return {
     props: {
-      posts
+      posts,
+      data
     }
   }
 }
 
-export default function Home({ posts }) {
+export default function Home({ posts, data }) {
   return (
     <div className={ styles.container }>
       <Head>
         <title>MnxBlog | ReadMe Solutions - minimalist NextJS readme blog</title>
         <meta charSet="utf-8" />
-        <meta name="description" content="README.md Solutions | nextjs minimalist readme blog, Feel free to fork it on github and make contributions...👋" key="desc"/>
-        <meta name="keyword" content="README, NextJS, TailwindCSS, CSS3, Markdown, Nodejs, Vercel, Minimalist"/>
+        <meta name="description" content="README.md Solutions | nextjs minimalist readme blog, Feel free to fork it on github and make contributions...👋" key="desc" />
+        <meta name="keyword" content="README, NextJS, TailwindCSS, CSS3, Markdown, Nodejs, Vercel, Minimalist" />
         <meta property="og:url" content="https://www.mnxblog.live" />
         <meta property="og:description" content="README.md Solutions | nextjs minimalist readme blog, Feel free to fork it on github and make contributions...👋" />
         <meta property="og:title" content="ReadMe files with guide to your problem" />
@@ -55,13 +59,38 @@ export default function Home({ posts }) {
         </div>
         <hr className='border' />
         <div className="max-w-max mb-10 mx-3 md:mx-auto">
+          <div className={ styles.cardshelf }>
+            <div className={ styles.cardshelf__header }>
+              <p className='md:inline'>Help is here. </p>
+              <span className='text-slate-500'>Whenever and however you need it.</span>
+            </div>
+            <div className='md:flex'>
+              <div className={ styles.childContainer }>
+                <div className={ styles.childWrapper }>
+                  <p>View Count</p>
+                  <p className={ styles.viewCount } id="viewCounter">
+                    { data.value ? data.value : "calculating..." }
+                  </p>
+                </div>
+              </div>
+              <div className={ styles.childContainer }>
+                <div className={ styles.childWrapper }>
+                  README is Awesome
+                  <Image src="/markdown.svg" alt="markdown" width={ 100 } height={ 100 } />
+                </div>
+                <div className={ styles.childWrapper }>
+                  Write your own readme Solution (feature is in progress)
+                </div>
+              </div>
+            </div>
+          </div>
           { posts?.map((post) => {
             return (
               <Link key={ `${post.slug}` } href={ `/blog/${post.slug}` }>
-                <div className={`${styles.borderGradient} p-5 hover:bg-slate-100 cursor-pointer select-none`}>
+                <div className={ `${styles.borderGradient} p-5 hover:bg-slate-100 cursor-pointer select-none` }>
                   <div key={ post.frontMatter.id } className='mb-4'>
-                    <span className='text-sm font-semibold text-slate-700'>{post.frontMatter.date}</span>
-                    <h1 className={`${styles.textGradient}  text-2xl font-bold text-slate-700`}>{ post.frontMatter.title }</h1>
+                    <span className='text-sm font-semibold text-slate-700'>{ post.frontMatter.date }</span>
+                    <h1 className={ `${styles.textGradient}  text-2xl font-bold text-slate-700` }>{ post.frontMatter.title }</h1>
                     <p className='mt-3 text-lg opacity-50 font-semibold'>{ post.frontMatter.desc }</p>
                   </div>
                 </div>
